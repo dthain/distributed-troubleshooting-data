399044c8b0f3c05052ec104a21b4d9790e081bf1
nkremerh <nkremerh@nd.edu>
2019-02-11 09:10:22 -0500

    Initial commit

---
+++ example/envtrace-helper.c
@@ -0,0 +1,338 @@
+#define _GNU_SOURCE
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <time.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <string.h>
+
+extern char * program_invocation_name;
+
+static FILE * logfile = 0;
+static const char * logfile_name = 0;
+static char * (*real_getenv) ( const char *name ) = 0;
+static int (*real_setenv) ( const char *name,const char *value,int overwrite ) = 0;
+static int (*real_unsetenv) ( const char *name ) = 0;
+static pid_t (*real_fork) ( ) = 0;
+static int (*real_clone) ( int (*fn)(void *),void *child_stack,int flags,void *arg,... ) = 0;
+static int (*real_open) ( const char *pathname,int flags,... ) = 0;
+static int (*real_creat) ( const char *pathname,mode_t mode ) = 0;
+
+char * getenv( const char *name )
+{
+	if(!real_getenv) {
+		real_getenv = dlsym(RTLD_NEXT,"getenv");
+		if(!real_getenv) {
+			fprintf(stderr,"envtrace-helper: couldn't find original getenv()\n");
+			exit(1);
+		}
+	}
+
+	if(!logfile_name) {
+		logfile_name = real_getenv("ENVTRACE_LOGFILE");
+		if(!logfile_name) {
+			fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+			exit(1);
+		}
+	}
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+	}
+
+	char *result = real_getenv(name);
+
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+	if(result) {	
+		fprintf(logfile,"%s@%s %ld.%ld: GETENV %ld %ld %s %s HIT %s\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,name,result);
+	} else {
+		fprintf(logfile,"%s@%s %ld.%ld: GETENV %ld %ld %s %s MISS\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,name);
+	}
+	fflush(logfile);
+
+	return result;
+}
+
+int setenv( const char *name,const char *value,int overwrite )
+{
+	if(!real_setenv) {
+		real_setenv = dlsym(RTLD_NEXT,"setenv");
+		if(!real_setenv) {
+			fprintf(stderr,"envtrace-helper: couldn't find original setenv()\n");
+			exit(1);
+		}
+	}
+
+	if(!logfile_name) {
+		logfile_name = getenv("ENVTRACE_LOGFILE");
+		if(!logfile_name) {
+			fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+			exit(1);
+		}
+	}
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+	}
+
+	const char *prev_val = getenv(name);
+	int result = real_setenv(name,value,overwrite);
+
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+	fprintf(logfile,"%s@%s %ld.%ld: SETENV %ld %ld %s %s %s %s %d\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,name,prev_val,value,result);
+	fflush(logfile);
+
+	return result;
+
+}
+
+int unsetenv( const char *name )
+{
+	if(!real_unsetenv) {
+		real_unsetenv = dlsym(RTLD_NEXT,"unsetenv");
+		if(!real_unsetenv) {
+			fprintf(stderr,"envtrace-helper: couldn't find original unsetenv()\n");
+			exit(1);
+		}
+	}
+
+	if(!logfile_name) {
+		logfile_name = getenv("ENVTRACE_LOGFILE");
+		if(!logfile_name) {
+			fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+			exit(1);
+		}
+	}
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+	}
+
+	const char *prev_val = getenv(name);
+	int result = real_unsetenv(name);
+
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+	fprintf(logfile,"%s@%s %ld.%ld: UNSET %ld %ld %s %s %s %d\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,name,prev_val,result);
+	fflush(logfile);
+
+	return result;
+
+}
+
+pid_t fork()
+{
+	if(!real_fork) {
+		real_fork = dlsym(RTLD_NEXT,"fork");
+		if(!real_fork) {
+			fprintf(stderr,"envtrace-helper: couldn't find original fork()\n");
+			exit(1);
+		}
+	}
+
+	if(!logfile_name) {
+		logfile_name = getenv("ENVTRACE_LOGFILE");
+		if(!logfile_name) {
+			fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+			exit(1);
+		}
+	}
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+	}
+	
+	pid_t result = real_fork();
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+	fprintf(logfile,"%s@%s %ld.%ld: FORK %ld %ld %s %"PRId64"\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,(int64_t) result);
+	fflush(logfile);
+
+	return result;
+}
+
+int clone( int (*fn)(void *),void *child_stack,int flags,void *arg,.../* pid_t *ptid, void *newtls, pid_t *ctid */ )
+{
+	if(!real_clone) {
+		real_clone = dlsym(RTLD_NEXT,"clone");
+		if(!real_clone) {
+			fprintf(stderr,"envtrace-helper: couldn't find original clone()\n");
+			exit(1);
+		}
+	}
+
+	if(!logfile_name) {
+		logfile_name = getenv("ENVTRACE_LOGFILE");
+		if(!logfile_name) {
+			fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+			exit(1);
+		}
+	}
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+	}
+	
+	int result = real_clone(fn,child_stack,flags,arg);
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+	fprintf(logfile,"%s@%s %ld.%ld: CLONE %ld %ld %s %d\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,result);
+	fflush(logfile);
+
+	return result;
+}
+
+
+int open( const char *pathname,int flags,... )
+{
+	if(!real_open) {
+		real_open = dlsym(RTLD_NEXT,"open");
+		if(!real_open) {
+			fprintf(stderr,"envtrace-helper: couldn't find original open()\n");
+			exit(1);
+		}
+	}
+
+	if(!logfile_name) {
+		logfile_name = getenv("ENVTRACE_LOGFILE");
+		if(!logfile_name) {
+			fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+			exit(1);
+		}
+	}
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+	}
+
+	va_list ap;
+	int mode;
+	va_start(ap, flags);
+	mode = va_arg(ap, int);
+	va_end(ap);
+
+	int result = real_open(pathname,flags,mode);
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+	fprintf(logfile,"%s@%s %ld.%ld: OPEN %ld %ld %s %s %d\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,pathname,result);
+	fflush(logfile);
+
+	return result;
+}
+
+int creat( const char *pathname,mode_t mode )
+{
+        if(!real_creat) {
+                real_creat = dlsym(RTLD_NEXT,"creat");
+                if(!real_creat) {
+                        fprintf(stderr,"envtrace-helper: couldn't find original creat()\n");
+                        exit(1);
+                }
+        }
+
+        if(!logfile_name) {
+                logfile_name = getenv("ENVTRACE_LOGFILE");
+                if(!logfile_name) {
+                        fprintf(stderr,"envtrace-helper: ENVTRACE_LOGFILE is not set.\n");
+                        exit(1);
+                }
+        }
+
+        if(!logfile) {
+                logfile = fopen(logfile_name,"a");
+                if(!logfile) {
+                        fprintf(stderr,"envtrace-helper: couldn't open %s for logging: %s\n",(char *)logfile,strerror(errno));
+                        exit(1);
+                }
+        }
+
+        int result = real_creat(pathname,mode);
+        pid_t pid = getpid();
+        pid_t ppid = getppid();
+	struct timespec timestamp;
+	clock_gettime(CLOCK_REALTIME,&timestamp);
+	char *user = real_getenv("USER");
+	char *hostname = malloc(sizeof(INT_MAX));
+	size_t hostsize = INT_MAX;
+	gethostname(hostname,hostsize);
+
+        fprintf(logfile,"%s@%s %ld.%ld: CREAT %ld %ld %s %s %d\n",user,hostname,(long)timestamp.tv_sec,timestamp.tv_nsec,(long)ppid,(long)pid,program_invocation_name,pathname,result);
+        fflush(logfile);
+
+        return result;
+
+}
---
+++ example/tasks.c
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2016- The University of Notre Dame
+ * This software is distributed under the GNU General Public License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+#include "work_queue.h"
+#include "work_queue_internal.h"
+#include "stringtools.h"
+#include "list.h"
+#include "debug.h"
+
+int capacity_test(int tasks, int sleep_time, char *name) {
+
+	//char *debug_file = string_format("%s.debug", name);
+	//char *log_file = string_format("%s.wqlog", name);
+	char *debug_file = "master.debug";
+	char *log_file = "master.wqlog";
+	cctools_debug_flags_set("all");
+	cctools_debug_config_file(debug_file);
+	cctools_debug_config_file_size(0);
+	
+	struct work_queue *wq = work_queue_create(-1);
+	if(!wq) {
+		fprintf(stderr, "Error in setting up Work Queue: %d - %s\n", errno, strerror(errno));
+		return 1;
+	}
+
+	printf("Listening on port %d...\n", work_queue_port(wq));
+	work_queue_specify_master_mode(wq, WORK_QUEUE_MASTER_MODE_CATALOG);
+	work_queue_specify_name(wq, name);
+
+	struct work_queue_stats s;
+	work_queue_specify_log(wq, log_file);
+	struct timeval curr_time;
+	gettimeofday(&curr_time, NULL);
+	int start_time = (int) curr_time.tv_sec;
+	int time_acc = 0;
+	int i = 0;
+	int taskid = 0;
+	int result = 0;
+	int complete = 0;
+	int failed = 0;
+	int timeout = ((sleep_time * 10) * tasks) + 10;
+
+	while(time_acc < timeout) {
+
+		gettimeofday(&curr_time, NULL);
+		time_acc = ((int) curr_time.tv_sec - start_time);
+		work_queue_get_stats(wq, &s);
+
+		while(i < tasks) {
+			char *out = string_format("out.%d.dat", i + 1);
+			char *env = string_format("env.%d.trace", i + 1);
+			char *wdb = string_format("task.%d.debug", i + 1);
+			char *cmd = string_format("make -f envtrace_makefile && ./envtrace dd if=in.dat of=out.dat bs=4096 count=2500 && sleep %d && cp ../../worker.debug %s", sleep_time, wdb);
+			struct work_queue_task *t = work_queue_task_create(cmd);
+
+			work_queue_task_specify_file(t, "in.dat", "in.dat", WORK_QUEUE_INPUT, WORK_QUEUE_CACHE);
+			work_queue_task_specify_file(t, "envtrace", "envtrace", WORK_QUEUE_INPUT, WORK_QUEUE_CACHE);
+			work_queue_task_specify_file(t, "envtrace_makefile", "envtrace_makefile", WORK_QUEUE_INPUT, WORK_QUEUE_CACHE);
+			work_queue_task_specify_file(t, "envtrace-helper.c", "envtrace-helper.c", WORK_QUEUE_INPUT, WORK_QUEUE_CACHE);
+			work_queue_task_specify_file(t, out, "out.dat", WORK_QUEUE_OUTPUT, WORK_QUEUE_NOCACHE);
+			work_queue_task_specify_file(t, env, "env.trace", WORK_QUEUE_OUTPUT, WORK_QUEUE_NOCACHE);
+			work_queue_task_specify_file(t, wdb, wdb, WORK_QUEUE_OUTPUT, WORK_QUEUE_NOCACHE);
+			work_queue_task_specify_max_retries (t, 0);	
+
+			taskid = work_queue_submit(wq, t);
+			fprintf(stderr, "Task %d submitted.\n", taskid);	
+			free(out);
+			free(cmd);
+			i++;
+		}
+
+		struct work_queue_task *t = work_queue_wait(wq, WORK_QUEUE_WAITFORTASK);
+		if(t) {	
+			if(t->result == WORK_QUEUE_RESULT_SUCCESS) {
+				fprintf(stderr, "Task %d complete. Collecting output.\n", t->taskid);
+				struct work_queue_file *out_file;
+				out_file = list_pop_head(t->output_files);
+				result = remove(out_file->payload);
+				if(result) {
+					fprintf(stderr, "Error in garbage collection of file %s: %d - %s.\n", out_file->payload, errno, strerror(errno));
+				}
+				//out_file = list_pop_head(t->output_files);
+				//result = remove(out_file->payload);
+				//if(result) {
+				//	fprintf(stderr, "Error in garbage collection of file %s: %d - %s.\n", out_file->payload, errno, strerror(errno));
+				//}
+				work_queue_task_delete(t);
+				complete++;
+			}
+			else {
+				//fprintf(stderr, "Task %d failed. Resubmitting.\n", t->taskid);
+				//work_queue_submit(wq, t);
+				fprintf(stderr, "Task %d failed.\n", t->taskid);
+				failed++;
+			}
+		}
+
+		if(complete + failed >= tasks) {
+			break;
+		}
+	}
+	work_queue_shut_down_workers(wq, 0);
+	work_queue_delete(wq);
+	fprintf(stderr, "Work Queue shut down after %d seconds with %d tasks scheduled and %d tasks completed.\n", time_acc, i, complete);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+	if(argc < 3) {
+		fprintf(stderr, "Must provide number of tasks to schedule, execution time per task, and  master name pattern.\n");
+		fprintf(stderr, "Example: 100 20 wq_capacity_5\n");
+		exit(1);
+	}
+	
+	int tasks = atoi(argv[1]);
+	int sleep_time = atoi(argv[2]);
+	char *name = argv[3];
+	capacity_test(tasks, sleep_time, name);
+
+	fprintf(stderr, "Testing complete.\n");
+	return 0;
+}
+/* vim: set noexpandtab tabstop=4: */
